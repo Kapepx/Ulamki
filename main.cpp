168a645f3bcd00dcfbf104fe31217c7b7ad0c007
Kapepx <98754618+Kapepx@users.noreply.github.com>
2023-01-25 23:06:34 +0100

    Podstawowe Set i Get składowych, dodawanie ułamek + double

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 class Fraction
 {
+public:
     struct
     {
         unsigned char valType : 1;
@@ -20,7 +21,7 @@ class Fraction
         } val;
     } num, denom;
 
-public:
+
     Fraction()
     {
         num.val.dblVal = 1;
@@ -32,6 +33,7 @@ public:
     Fraction(double p_whole)
     {
         num.val.dblVal = p_whole;
+        num.valType = FRAC_TYPE_DOUBLE;
         denom.val.dblVal = 1;
         denom.valType = FRAC_TYPE_DOUBLE;
     }
@@ -46,7 +48,7 @@ public:
         {
             denom.val.dblVal = 1;
             #ifdef FRAC_PRINT_ERRORS
-            printf("Mianownik nie mo¿e byæ równy 0");
+            printf("Mianownik nie moÅ¼e byÄ‡ rÃ³wny 0");
             #endif // FRAC_PRINT_ERRORS
         }
         denom.valType = FRAC_TYPE_DOUBLE;
@@ -72,6 +74,134 @@ public:
 
         num.val.dblVal = p_whole * denom.val.dblVal + p_num;
         num.valType = FRAC_TYPE_DOUBLE;
+    }
+
+    void Print()
+    {
+        printf("Fraction: %lf / %lf = %lf\n", GetNumAsDbl(), GetDenomAsDbl(), GetValue());
+    }
+
+    void SetNum(double val)
+    {
+        num.valType = FRAC_TYPE_DOUBLE;
+        num.val.dblVal = val;
+    }
+
+    void SetNum(Fraction &Frac)
+    {
+        num.valType = FRAC_TYPE_FRAC;
+        num.val.fracVal = new Fraction();
+        num.val.fracVal->SetNum(Frac.GetNumAsDbl());
+    }
+
+    void SetDenom(double val)
+    {
+        if (val == 0)
+        {
+            #ifdef FRAC_PRINT_ERRORS
+            printf("Mianownik nie moze byc rowny 0");
+            #endif // FRAC_PRINT_ERRORS
+            return;
+        }
+
+        denom.valType = FRAC_TYPE_DOUBLE;
+        denom.val.dblVal = val;
+    }
+
+    double GetNumAsDbl()
+    {
+        if (num.valType == FRAC_TYPE_DOUBLE)
+        {
+            return num.val.dblVal;
+        }
+        else
+        {
+            if (num.val.fracVal == 0)
+            {
+                #ifdef FRAC_PRINT_ERRORS
+                printf("Licznik wskazuje na nie istniejacy ulamek");
+                #endif // FRAC_PRINT_ERRORS
+                return 0;
+            }
+            else
+            {
+                return num.val.fracVal->GetValue();
+            }
+        }
+    }
+
+    double GetDenomAsDbl()
+    {
+        if (denom.valType == FRAC_TYPE_DOUBLE)
+        {
+            return denom.val.dblVal;
+        }
+        else
+        {
+            if (denom.val.fracVal == 0)
+            {
+                #ifdef FRAC_PRINT_ERRORS
+                printf("Licznik wskazuje na nie istniejacy ulamek");
+                #endif // FRAC_PRINT_ERRORS
+                return 0;
+            }
+            else
+            {
+                return denom.val.fracVal->GetValue();
+            }
+        }
+    }
+
+    //TODO
+    Fraction GetNumAsFrac()
+    {
+        if (num.valType == FRAC_TYPE_DOUBLE)
+        {
+            return Fraction(num.val.dblVal);
+        }
+        else
+        {
+            return *num.val.fracVal;
+        }
+    }
+
+    //TODO
+    Fraction GetDenomAsFrac()
+    {
+        if (denom.valType == FRAC_TYPE_DOUBLE)
+        {
+            return Fraction(denom.val.dblVal);
+        }
+        else
+        {
+            return *denom.val.fracVal;
+        }
+    }
+
+    void* GetNumRaw(unsigned char *out_type)
+    {
+        if (out_type == 0)
+        {
+            #ifdef FRAC_PRINT_ERRORS
+            printf("Nie przekazano zmiennej na przechowanie typu\n");
+            #endif
+        }
+
+        *out_type = num.valType;
+        return &num.val;
+    }
+
+    void* GetDenomRaw(unsigned char *out_type)
+    {
+        if (out_type == 0)
+        {
+            #ifdef FRAC_PRINT_ERRORS
+            printf("Nie przekazano zmiennej na przechowanie typu\n");
+            #endif // FRAC_PRINT_ERRORS
+        }
+
+        *out_type = num.valType;
+        return &num.val;
     }
 
     double GetValue()
@@ -116,12 +246,44 @@ public:
         }
     }
 
+    Fraction operator + (double val)
+    {
+        double trueDenom, trueNum;
+        if (denom.valType == FRAC_TYPE_FRAC)
+        {
+            trueDenom = denom.val.fracVal->GetDenomAsDbl();
+        }
+        else
+        {
+            trueDenom = denom.val.dblVal;
+        }
+        if (trueDenom == 0)
+        {
+            printf("Jest zle\n");
+            return Fraction();
+        }
+
+        if  (num.valType == FRAC_TYPE_FRAC)
+        {
+            trueNum = num.val.fracVal->GetNumAsDbl();
+        }
+        else
+        {
+            trueNum = num.val.dblVal;
+        }
+
+        return Fraction(trueNum + (val * trueDenom), trueDenom);
+    }
 };
 
 int main()
 {
     Fraction F1, F2(1, 2);
-    printf("F1: %Lf\n", F1.GetValue());
-    printf("F2: %Lf\n", F2.GetValue());
+
+    F1.Print();
+    F1.Print();
+
+    F2 = F2 + 5;
+    F2.Print();
     return 0;
 }
